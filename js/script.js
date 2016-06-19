e4c576efd8dcf698d9310fb918d358dc0390f519
houripj357 <houripj357@gmail.com>
2016-06-18 22:44:30 -0400

    add js directory and file with a few random functions

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function offer () {
+	alert('If you text me directly on my personal cell phone, (904)608-4634, I will write your first blog post for free!');
+}
+
+function youwantme () {
+	alert('you want me, you really want me');
+}
+
+function phone () {
+	alert('you want me, you really want me');
+}
